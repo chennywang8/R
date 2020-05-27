b603049a79391f94a5f1e93594894c5451ce1572
Chenny <lotharwang@gmail.com>
2020-05-27 14:59:35 -0700

    use dplyr for data processing

--- model3_seat/motor_position.R
+++ model3_seat/motor_position.R
@@ -1,10 +1,16 @@
 library(ggplot2)
 library(scales)
+library(dplyr)
 
-raw <- read.csv("model3_seat.csv")
-raw <- raw[raw$prop_value>60, ]
-raw <- raw[!(raw$prop_tag=="020123L"&raw$prop_value<100), ]
+fph <- "model3_seat_RH.csv"
+raw <- read.csv(fph) %>%
+       mutate(tag = substring(prop_tag, 1, 5)) %>%
+       filter((tag=="02003"&between(prop_value, 650, 800)) |
+              (tag=="02012"&between(prop_value, 100, 200)) |
+              (tag=="02006"&between(prop_value, 60, 100)) |
+              (tag=="02009"&between(prop_value, 700, 785)))
 raw$date_time <- as.Date(raw$date_time)
+
 
 ggplot(raw, aes(x = date_time, y = prop_value)) +
   facet_wrap(. ~ prop_tag , ncol = 2, scales = "free") +
@@ -17,3 +23,8 @@ ggplot(raw, aes(x = prop_value, color = prop_tag, fill = prop_tag)) +
   facet_wrap(. ~ prop_tag , ncol = 2, scales = "free") +
   theme_linedraw(base_size = 20) +
   geom_bar()
+
+ff <- read.csv(fph) %>%
+      filter(prop_value==0) %>%
+      group_by(prop_tag) %>%
+      summarise(n = n())
