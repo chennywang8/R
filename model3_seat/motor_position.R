198648781a6fae71d3f4c259e1c940dd14b4e706
Chenny <lotharwang@gmail.com>
2020-06-06 22:11:38 -0700

    update

--- model3_seat/motor_position.R
+++ model3_seat/motor_position.R
@@ -2,15 +2,16 @@ library(ggplot2)
 library(scales)
 library(dplyr)
 
-fph <- "model3_seat_RH.csv"
+fph <- "model3_seat_LH_66.csv"
 raw <- read.csv(fph) %>%
-       mutate(tag = substring(prop_tag, 1, 5)) %>%
+       mutate(tag = substring(prop_tag, 1, 5),
+              prop_value = as.double(prop_value),
+              date_time = as.Date(date_time)) %>%
        filter((tag=="02003"&between(prop_value, 650, 800)) |
               (tag=="02012"&between(prop_value, 100, 200)) |
               (tag=="02006"&between(prop_value, 60, 100)) |
-              (tag=="02009"&between(prop_value, 700, 785)))
-raw$date_time <- as.Date(raw$date_time)
-
+              (tag=="02009"&between(prop_value, 770, 785)))
+fff <- read.csv(fph) %>% filter(prop_status==0)
 
 ggplot(raw, aes(x = date_time, y = prop_value)) +
   facet_wrap(. ~ prop_tag , ncol = 2, scales = "free") +
@@ -24,7 +25,6 @@ ggplot(raw, aes(x = prop_value, color = prop_tag, fill = prop_tag)) +
   theme_linedraw(base_size = 20) +
   geom_bar()
 
-ff <- read.csv(fph) %>%
-      filter(prop_value==0) %>%
-      group_by(prop_tag) %>%
-      summarise(n = n())
+f_zero <- fff %>% filter(prop_value==0) %>% group_by(prop_tag) %>% summarise(n = n())
+
+f_fail <- fff %>% group_by(prop_tag) %>% summarise(n = n())
