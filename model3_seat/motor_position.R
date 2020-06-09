ad2e6da4874ba28271180615fd38be93c402255d
Chenny <lotharwang@gmail.com>
2020-06-09 09:54:44 -0700

    use lapply to process all csv files and

--- model3_seat/motor_position.R
+++ model3_seat/motor_position.R
@@ -2,17 +2,38 @@ library(ggplot2)
 library(scales)
 library(dplyr)
 
-fph <- "model3_seat_LH_66.csv"
-raw <- read.csv(fph) %>%
-       mutate(tag = substring(prop_tag, 1, 5),
-              prop_value = as.double(prop_value),
-              date_time = as.Date(date_time)) %>%
-       filter((tag=="02003"&between(prop_value, 650, 800)) |
-              (tag=="02012"&between(prop_value, 100, 200)) |
-              (tag=="02006"&between(prop_value, 60, 100)) |
-              (tag=="02009"&between(prop_value, 770, 785)))
-fff <- read.csv(fph) %>% filter(prop_status==0)
 
+# ======= FUNCTION READ RAW DATA ========
+funcReadCsv <- function(fph) {
+    read.csv(fph) %>%
+    mutate(tag = substring(prop_tag, 1, 5),
+           prop_value = as.double(prop_value),
+           date_time = as.Date(date_time)) %>%
+    filter((tag=="02003"&between(prop_value, 650, 800)) |
+             (tag=="02012"&between(prop_value, 100, 200)) |
+             (tag=="02006"&between(prop_value, 60, 100)) |
+             (tag=="02009"&between(prop_value, 765, 785)))
+}
+
+# ======== FETCH ALL DATA ==============
+raw <- list.files('.', pattern = ".csv",
+                  full.names = TRUE, recursive = TRUE) %>%
+       lapply(., funcReadCsv) %>%
+       do.call(rbind, .) %>%
+       unique()
+fff <- list.files('.', pattern = ".csv",
+                  full.names = TRUE, recursive = TRUE) %>%
+       lapply(., read.csv) %>%
+       do.call(rbind, .) %>%
+       filter(prop_status==0) %>%
+       unique()
+
+f_zero <- fff %>% filter(prop_value==0) %>%
+          group_by(prop_tag) %>% summarise(n = n())
+
+f_fail <- fff %>% group_by(prop_tag) %>% summarise(n = n())
+
+# ======== PLOT ========
 ggplot(raw, aes(x = date_time, y = prop_value)) +
   facet_wrap(. ~ prop_tag , ncol = 2, scales = "free") +
   theme_linedraw(base_size = 20) +
@@ -25,6 +46,3 @@ ggplot(raw, aes(x = prop_value, color = prop_tag, fill = prop_tag)) +
   theme_linedraw(base_size = 20) +
   geom_bar()
 
-f_zero <- fff %>% filter(prop_value==0) %>% group_by(prop_tag) %>% summarise(n = n())
-
-f_fail <- fff %>% group_by(prop_tag) %>% summarise(n = n())
