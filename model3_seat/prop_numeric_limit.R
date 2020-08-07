ba7d62418499d51ef5cf31b713bc436093f2c0e8
Chenny <lotharwang@gmail.com>
2020-08-06 19:02:23 -0700

    wrap up two functions to read and parse data

--- model3_seat/prop_numeric_limit.R
+++ model3_seat/prop_numeric_limit.R
@@ -2,24 +2,35 @@ library(ggplot2)
 library(scales)
 library(dplyr)
 
-file_path <- "Test Data 3.0.17 7-24-2020.csv"
-raw <- read.csv(file_path) %>%
-        mutate(tag = substring(PROP_TAG, 1, 5),
-               DATE_TIME = as.POSIXct(DATE_TIME))
-names(raw) <- tolower(names(raw))
-shp <- raw %>% select(-c("uut_name", "uut_part_number", "comp_operator", "step_name")) %>%
-      filter((tag=="02003"&between(prop_value, 670, 800)) |
+funcReadRaw <- function(file_path) {
+  raw <- read.csv(file_path) %>%
+          mutate(tag = substring(PROP_TAG, 1, 5),
+                 DATE_TIME = as.POSIXct(DATE_TIME))
+  names(raw) <- tolower(names(raw))
+  raw
+}
+
+funcFilterShp <- function(raw) {
+  raw %>% select(-c("uut_name", "uut_part_number", "comp_operator", "step_name")) %>%
+      filter((tag=="02003"&between(prop_value, 700, 800)) |
                (tag=="02012"&between(prop_value, 130, 200)) |
                (tag=="02006"&between(prop_value, 67, 100)) |
-               (tag=="02009"&between(prop_value, 750, 785)) )
+               (tag=="02009"&between(prop_value, 765, 785)) )
+}
 
+
+# ======== MAIN ========
+f1 <- "Test Data 3.0.15 7-25-2020 to 8-6-2020.csv"
+f2 <- "Test Data 3.0.17 7-24-2020.csv"
+raw <- rbind(funcReadRaw(f1), funcReadRaw(f2))
+shp <- funcFilterShp(raw)
 recline <- shp %>% filter(tag=="02003"&!between(prop_value, 713, 719))
 
 # ======== PLOT ========
 ggplot(shp, aes(x = date_time, y = prop_value)) +
   facet_wrap(. ~ prop_tag, ncol = 2, scales = "free") +
   theme_linedraw(base_size = 20) +
-  geom_point(aes(color = prop_tag)) +
+  geom_point(aes(color = prop_tag, shape = sw_version)) +
   labs(x="Test Date", y = "Shipping Position")
 
 
