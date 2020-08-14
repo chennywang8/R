196d8d97a351495799ff331b24d55d251d1d6d7a
Chenny <lotharwang@gmail.com>
2020-08-14 12:52:01 -0700

    add some history data for sw_version comparison

--- model3_seat/prop_numeric_limit.R
+++ model3_seat/prop_numeric_limit.R
@@ -20,34 +20,51 @@ funcFilterShp <- function(raw) {
 
 
 # ======== MAIN ========
-f1 <- "Test Data 3.0.15 7-25-2020 to 8-6-2020.csv"
-f2 <- "Test Data 3.0.17 7-24-2020.csv"
-raw <- rbind(funcReadRaw(f1), funcReadRaw(f2))
+freg<- "*.csv"
+raw <- list.files('./data_with_version/', pattern = freg,
+                  full.names = TRUE, recursive = TRUE) %>%
+  lapply(., funcReadRaw) %>%
+  do.call(rbind, .) %>%
+  unique()
 shp <- funcFilterShp(raw)
 recline <- shp %>% filter(tag=="02003"&!between(prop_value, 713, 719))
+cdr <- raw %>% filter(tag %in% c('02133', '02134', '02131', '02132'))
 
 # ======== PLOT ========
 ggplot(shp, aes(x = date_time, y = prop_value)) +
   facet_wrap(. ~ prop_tag, ncol = 2, scales = "free") +
   theme_linedraw(base_size = 20) +
-  geom_point(aes(color = prop_tag, shape = sw_version)) +
+  geom_point(aes(color = sw_version)) +
   labs(x="Test Date", y = "Shipping Position")
 
 
-ggplot(shp, aes(x = prop_value, color = prop_tag, fill = prop_tag)) +
+ggplot(shp, aes(x = prop_value, color = sw_version, fill = sw_version)) +
   facet_wrap(. ~ prop_tag, ncol = 2, scales = "free") +
   theme_linedraw(base_size = 20) +
   geom_bar()
 
+ggplot(cdr, aes(x = date_time, y = prop_value)) +
+  facet_wrap(. ~ prop_tag , ncol = 2, scales = "free") +
+  theme_linedraw(base_size = 20) +
+  geom_point(aes(color = sw_version)) +
+  labs(x="Test Date", y = "Current Draw [A]")
+
+
+ggplot(cdr, aes(x = prop_value, color = sw_version, fill = sw_version)) +
+  facet_wrap(. ~ prop_tag , ncol = 2, scales = "free") +
+  theme_linedraw(base_size = 20) +
+  geom_bar()
 
 # ======= LASER ========
 lt1 <- filter(raw, tag=="02105"&step_status!="Passed") %>%
   left_join(filter(raw, tag=="02005"), by = "date_time") %>%
   left_join(filter(raw, tag=="02006"), by = "date_time") %>%
-  select(date_time, prop_tag.x, prop_value.x, prop_tag.y, prop_value.y, prop_tag, prop_value) %>%
+  select(date_time, prop_tag.x, prop_value.x, prop_tag.y, prop_value.y,
+         prop_tag, prop_value, sw_version, station_id) %>%
   arrange(date_time)
 names(lt1) <- c("date_time", "laser_tag", "laser_reading",
                 "second_run_tag", "second_run_encoder",
-                "third_run_tag", "third_run_encoder")
+                "third_run_tag", "third_run_encoder",
+                "software_version", "station_id")
 
 lt2 <- lt1 %>% filter(third_run_encoder!=0)
